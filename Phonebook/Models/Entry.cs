b0faf99bde27791a6ecfae96f48959354064297c
prajeshJugga <prajesh.jugga@gmail.com>
2022-03-09 23:31:16 +0200

    Add project files.

---
+++ Phonebook/Models/Entry.cs
@@ -0,0 +1,9 @@
+﻿namespace Phonebook.Models
+{
+    public class Entry
+    {
+        public string PhoneBookName { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+    }
+}
---
+++ Phonebook/Models/PhoneBook.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Phonebook.Models
+{
+    public class PhoneBook
+    {
+        public string Name { get; set; }
+        public List<Entry> Entries { get; set; }
+    }
+}
