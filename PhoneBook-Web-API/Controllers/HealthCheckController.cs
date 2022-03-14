e96214f9ee3f51fdebfc19a8fb89e7931dc8c110
prajeshJugga <prajesh.jugga@gmail.com>
2022-03-14 11:33:32 +0200

    Removed default controllers and added HealthCheck controller

---
+++ PhoneBook-Web-API/Controllers/HealthCheckController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBook_Web_API.Controllers
+{
+    [ApiController]
+    [Route("healthcheck")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Healthy";
+        }
+    }
+}
